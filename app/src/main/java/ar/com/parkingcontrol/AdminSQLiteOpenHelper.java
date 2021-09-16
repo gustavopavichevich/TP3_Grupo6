3c9d2b8408ed37ae7cda58540b5bf0b1dcb1b4a5
Gustavo Pavichevich <88042886+guspavichevich-globant@users.noreply.github.com>
2021-09-16 02:46:23 -0300

    (upgrade): Actualizacióm de clase AdminSQLiteOpenHeler

--- app/src/main/java/ar/com/parkingcontrol/AdminSQLiteOpenHelper.java
+++ app/src/main/java/ar/com/parkingcontrol/AdminSQLiteOpenHelper.java
@@ -1,6 +1,25 @@
 package ar.com.parkingcontrol;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class AdminSQLiteOpenHelper {
+import androidx.annotation.Nullable;
 
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase BaseDeDatos) {
+        BaseDeDatos.execSQL("create table usuarios(idUsuario int primary key, usuario)");
+        BaseDeDatos.execSQL("create table parqueos(idparqueo int primary key, usuario)");
+        BaseDeDatos.execSQL("create table usuarios(idUsuario int primary key, usuario)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
--- app/src/main/java/ar/com/parkingcontrol/MainActivity.java
+++ app/src/main/java/ar/com/parkingcontrol/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        txtId
         setContentView(R.layout.activity_main);
     }
     public void Registrar (View view)
