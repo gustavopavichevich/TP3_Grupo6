14a360039e53cf13e4818884fc85a10a6d5af5da
German Medina <gmedinasistemas@gmail.com>
2021-09-16 13:30:20 -0300

    Agrego Funcionalidad al activity_registro
    //backend
    //Méotdo para dar de alta al nuevo usuario de la app
    public void Registrar
    //Valido que no existe el email/usuario en la base de datos
    public boolean ValidarUsuario
    
    Valido Contraseñas.
    
    //frontend
        <string name="txtNombre"> Nombre</string>
        <string name="txtCorreo"> Correo electrónico</string>
        <string name="txtPass"> Contraseña</string>
        <string name="txtRePass"> Repetir Contraseña</string>
    
    agrego hint en activity_registro.xml y en activity_main

--- app/src/main/java/ar/com/parkingcontrol/AdminSQLiteOpenHelper.java
+++ app/src/main/java/ar/com/parkingcontrol/AdminSQLiteOpenHelper.java
@@ -13,7 +13,7 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase BaseDeDatos) {
-        BaseDeDatos.execSQL("create table usuarios(idUsuario int primary key, usuario, contrasenia)");
+        BaseDeDatos.execSQL("create table usuarios(idUsuario int primary key, nombre, usuario, contrasenia)");
     }
 
     @Override
